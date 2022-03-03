c993c6241040bfdd8b088fca38b89d8383bcddfa
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-03-03 10:47:11 +0100

    Create user and sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to :tasks, notice: 'Logged In, successfully!'
+    else
+      flash[:notice] = 'Unable to login, can not verify the user!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged Out, Goodbye!'
+  end
+end
