4bc6091d9b3f1cbebab159ebcb9875e4313c0e52
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-03-03 10:58:35 +0100

    Add session routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+
+  get 'sessions', to: 'sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
